29c93392c42e7ef073d6ea0a3527a7d4136630d7
yokovaski <erwinl1992@hotmail.com>
2017-12-12 22:14:54 +0100

    Fix for scaling down canvas size on redraw

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,7 +71,7 @@
                                 <div class="clearfix"></div>
                             </div>
                             <div class="panel-body">
-                                <canvas id="chartEnergyUse"></canvas>
+                                <canvas id="chartEnergyUse" style="height:370px"></canvas>
                             </div>
                         </div>
                     </div>
@@ -92,7 +92,7 @@
                                 <div class="clearfix"></div>
                             </div>
                             <div class="panel-body">
-                                <canvas id="chartEnergySolar"></canvas>
+                                <canvas id="chartEnergySolar" style="height:370px"></canvas>
                             </div>
                         </div>
                     </div>
@@ -113,7 +113,7 @@
                                 <div class="clearfix"></div>
                             </div>
                             <div class="panel-body">
-                                <canvas id="chartEnergyRedelivery"></canvas>
+                                <canvas id="chartEnergyRedelivery" style="height:370px"></canvas>
                             </div>
                         </div>
                     </div>
@@ -134,7 +134,7 @@
                                 <div class="clearfix"></div>
                             </div>
                             <div class="panel-body">
-                                <canvas id="chartEnergyIntake"></canvas>
+                                <canvas id="chartEnergyIntake" style="height:370px"></canvas>
                             </div>
                         </div>
                     </div>
