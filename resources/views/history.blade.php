0548cc0e2d8c16852d00fb2e545212a61f5b4195
yokovaski <erwinl1992@hotmail.com>
2018-06-17 18:14:53 +0200

    Fix for layout history view

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <div class="col-md-9">
                 <div class="card mb-4">
